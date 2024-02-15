732b4f9a087fdc1251fad12481126b717def14d3
Jayash <jayashysaini7361@gmail.com>
2024-02-16 00:07:47 +0530

    "Initilaizing Vite"

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Components/Header";
+function App() {
+  return (
+    <div className=" h-screen w-full bg-slate-800">
+      <Header />
+      <p className="text-red-200"> HELLO DOSTO</p>
+    </div>
+  );
+}
+
+export default App;
