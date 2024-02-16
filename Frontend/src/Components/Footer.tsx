713df3b0ef0a6a7ab1a5e380c1250ff13006a8aa
Jayash <jayashysaini7361@gmail.com>
2024-02-16 23:02:59 +0530

    "Add Header Footer Components"

---
+++ Frontend/src/Components/Footer.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import CookieLogo from "../assets/WhiteCookie.svg";
+import appStoreIMG from "../assets/asset 21.png";
+import playStoreIMG from "../assets/asset 22.png";
+const Footer = () => {
+  const [email, setEmail] = useState("");
+  return (
+    <>
+      <div className="hidden lg:flex w-full justify-evenly items-center gap-5 bg-black p-14">
+        <div className="m-2 p-5">
+          <img
+            src={CookieLogo}
+            alt="cookie"
+            width="220px"
+            className="cursor-pointer"
+          />
+
+          <h3 className="text-white mt-5 mb-5 text-xl">
+            Taste the Difference!
+          </h3>
+
+          <div className="flex gap-2 items-center justify-evenly">
+            <img
+              src={appStoreIMG}
+              alt="appstore"
+              width="100px"
+              className="bg-slate-900 rounded-md"
+            />
+            <img
+              src={playStoreIMG}
+              alt="playstore"
+              width="100px"
+              className="bg-slate-900 rounded-md"
+            />
+          </div>
+        </div>
+        <div className="m-2 p-5  w-[400px]">
+          <h2 className="font-extrabold  text-white text-4xl mb-5">INFO</h2>
+          <ul className="columns-2 font-semibold text-white text-[18px]">
+            <li className="hover:text-[#ff9330] duration-200 ease-in-out cursor-pointer">
+              About Us
+            </li>
+            <li className="hover:text-[#ff9330] duration-200 ease-in-out cursor-pointer">
+              Career
+            </li>
+            <li className="hover:text-[#ff9330] duration-200 ease-in-out cursor-pointer">
+              Contact Us
+            </li>
+            <li className="hover:text-[#ff9330] duration-200 ease-in-out cursor-pointer">
+              Terms & Conditions
+            </li>
+            <li className="hover:text-[#ff9330] duration-200 ease-in-out cursor-pointer">
+              Catering
+            </li>
+            <li className="hover:text-[#ff9330] duration-200 ease-in-out cursor-pointer">
+              Franchise
+            </li>
+            <li className="hover:text-[#ff9330] duration-200 ease-in-out cursor-pointer">
+              Nutritional & Allergy Information
+            </li>
+          </ul>
+        </div>
+        <div className=" w-1/3 m-2 p-2 ">
+          <h1 className=" font-extrabold  text-white text-4xl mb-5 leading-tight cursor-pointer">
+            <span className="font-[1000] text-[#ff9330]">SUBSCRIBE</span> TO OUR
+            EMAILS.
+          </h1>
+          <div className="flex justify-center items-center">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              className="w-64 px-4 py-2 border font-semibold text-black border-gray-300 rounded-l focus:outline-none"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button className="px-4 py-2 bg-orange-500 text-white rounded-r font-bold hover:bg-orange-600 focus:outline-none">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- Frontend/src/Components/Header.tsx
+++ Frontend/src/Components/Header.tsx
@@ -2,10 +2,44 @@ import cookieImage from "../assets/asset 0.svg";
 import Signup from "../assets/LoginLogo.svg";
 import CookieCount from "../assets/asset 2.svg";
 import Hamburgur from "../assets/Hamburgur.svg";
+import locationWhite from "../assets/LocationWhite.svg";
+import phoneLogo from "../assets/phoneLogo.svg";
+import { useState, useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const Header = () => {
+  const container = useRef(null);
+  const { contextSafe } = useGSAP({ scope: container });
+  const [menuState, setMenuState] = useState(false);
+  const hamburgerHandler = contextSafe(() => {
+    if (!menuState) {
+      gsap.to(".hamburger", {
+        display: "block",
+        duration: 0.5,
+        y: 0,
+        opacity: 1,
+      }); // Slide container from bottom to top
+      setMenuState(true);
+    } else {
+      gsap.to(".hamburger", {
+        display: "none",
+        duration: 0.5,
+        y: 500,
+        opacity: 0,
+      }); // Slide container from top to bottom
+      setMenuState(false);
+    }
+  });
+
+  const totalHeight = window.innerHeight; // Get the total height of the window
+  const minHeight = `calc(${totalHeight}px - 300px)`; // Calculate min-height
+
   return (
     <>
-      <div className="bg-white pt-7 pb-7 pl-6 pr-6 md:pl-16 md:pr-16 w-full">
+      <div
+        className="bg-white pt-7 pb-7 pl-6 pr-6 md:pl-16 md:pr-16 w-full relative"
+        ref={container}>
         <div className="flex justify-start items-center w-full">
           <img
             src={cookieImage}
@@ -28,25 +62,95 @@ const Header = () => {
               <img
                 src={Signup}
                 alt="Signup"
-                height="40px"
-                width="40px"
-                className="cursor-pointer"
+                height="35px"
+                width="35px"
+                className="cursor-pointer  md:w-10 lg:w-12"
               />
               <img
                 src={CookieCount}
                 alt="Cookie"
                 height="35px"
                 width="35px"
-                className="cursor-pointer"
+                className="cursor-pointer  md:w-10 lg:w-12"
               />
               <img
-                src={Signup}
-                alt="Signup"
-                height="40px"
-                width="40px"
-                className="cursor-pointer"
+                src={Hamburgur}
+                alt="Hamburger"
+                height="35px"
+                width="35px"
+                onClick={hamburgerHandler}
+                className="cursor-pointer  md:w-40px lg:w-35px sm:hidden block"
               />
             </div>
+          </div>
+        </div>
+
+        <div className=" hamburger sm:hidden  h-auto  hidden w-full absolute top-0 left-0   backdrop-blur-sm bg-slate-500/30 translate-y-full opacity-0">
+          <div
+            className=" sm:hidden h-[100px] w-full"
+            onClick={hamburgerHandler}></div>
+
+          <div className=" sm:hidden  h-[200px] w-full bg-black flex flex-col justify-center items-center">
+            <div className="flex justify-evenly items-center gap-3">
+              <img src={locationWhite} alt="location" />
+              <h3 className="font-medium text-white text-xl">Store Location</h3>
+              <h4 className="font-bold text-[#ff9330]">Change</h4>
+            </div>
+            <div>
+              <h3 className="font-medium text-white text-sm text-center m-2">
+                Rajasthan Jaipur Sodala <br /> Govindpuri D-77
+              </h3>
+            </div>
+            <div className="flex justify-evenly items-center gap-1">
+              <img src={phoneLogo} alt="phone" height="30px" width="25px" />
+              <h3 className="font-medium text-white text-xl text-center m-2">
+                9782466675
+              </h3>
+              <a href="*" className="font-bold  text-[#ff9330]">
+                Email
+              </a>
+            </div>
+          </div>
+
+          <div
+            className=" sm:hidden min-h-calc(totalheight - 300px)  w-full bg-white flex flex-col justify-start items-center p-5 relative"
+            style={{ minHeight }}>
+            <h2 className="font-bold text-black text-xl">
+              Looking for a delivery?
+            </h2>
+            <p className="font-bold text-slate-600 text-sm mb-10">
+              Visit one of our partner site
+            </p>
+            <a
+              href="*"
+              className="m-2 font-bold text-black text-[18px] text-center">
+              About Us
+            </a>
+            <a
+              href="*"
+              className="m-2 font-bold text-black text-[18px] text-center">
+              Careers
+            </a>
+            <a
+              href="*"
+              className="m-2 font-bold text-black text-[18px] text-center">
+              Contact Us
+            </a>
+            <a
+              href="*"
+              className="m-2 font-bold text-black text-[18px] text-center">
+              Terms & Conditions
+            </a>
+            <a
+              href="*"
+              className="m-2 font-bold text-black text-[18px] text-center">
+              Franchise
+            </a>
+            <a
+              href="*"
+              className="m-2 font-bold text-black text-[18px] text-center">
+              Nutritional & Allergy Informations
+            </a>
           </div>
         </div>
       </div>
