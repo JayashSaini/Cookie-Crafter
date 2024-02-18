1778fa2746e450bb527eb7c20daad3b3b8b39e3b
Jayash <jayashysaini7361@gmail.com>
2024-02-18 20:02:54 +0530

    "Add react router dom"

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,12 +2,19 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className=" h-screen w-full ">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     </div>
   );
 }
