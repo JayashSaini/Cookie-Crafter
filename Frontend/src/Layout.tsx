ed10e97aa8af3e84d9c7999d9db10113c5557d6e
Jayash <jayashysaini7361@gmail.com>
2024-02-26 00:33:29 +0530

    "Add Scroll event listener"

--- Frontend/src/Layout.tsx
+++ Frontend/src/Layout.tsx
@@ -1,13 +1,30 @@
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import MyProvider from "./context/ContextProvider.jsx";
 
-const Layout = () => {
+const Layout: React.FC = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
       <MyProvider>
-        <Header />
+        <Header isScrolled={isScrolled} />
         <Outlet />
         <Footer />
       </MyProvider>
