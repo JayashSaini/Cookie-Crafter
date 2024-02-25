2840c31cd945322b2a8a80b95d6cc1991070021e
Jayash <jayashysaini7361@gmail.com>
2024-02-26 00:34:49 +0530

    "Add inputData interface"

--- Frontend/src/types/types.ts
+++ Frontend/src/types/types.ts
@@ -2,3 +2,11 @@ export interface MyContextData {
   hamburgerState: boolean;
   toggleHamburger: () => void;
 }
+
+export interface InputData {
+  label: string;
+  type?: string;
+  value: string | number;
+  style: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
