a2537deac496915a34d7b7e15253b6623ef53469
Jayash <jayashysaini7361@gmail.com>
2024-02-26 00:34:24 +0530

    "Import all pages in index.ts"

---
+++ Frontend/src/Pages/index.ts
@@ -0,0 +1,6 @@
+import Home from "./Home/Home.tsx";
+import Franchise from "./Franchise/Franchise.tsx";
+import Catering from "./Catering/Catering.tsx";
+import Location from "./Location/Location.tsx";
+
+export { Home, Franchise, Catering, Location };
