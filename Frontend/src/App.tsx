f77523ee874d5a89915bd78d16a138d60e71d395
Jayash <jayashysaini7361@gmail.com>
2024-02-26 00:32:53 +0530

    "Add Location Catering Route"

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
-import Layout from "./Layout.tsx";
-import Home from "./Pages/Home/Home.tsx";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { Home, Franchise, Catering, Location } from "./Pages";
+import Layout from "./Layout.tsx";
+
 const App: React.FC = () => {
   return (
     <div className=" h-screen w-full ">
@@ -11,6 +12,9 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="/franchise" element={<Franchise />} />
+            <Route path="/location" element={<Location />} />
+            <Route path="/catering" element={<Catering />} />
           </Route>
         </Routes>
       </Router>
