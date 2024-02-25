e4661c46c5de5b4c72588e8cdde6fe65c6e00efa
Jayash <jayashysaini7361@gmail.com>
2024-02-25 16:26:15 +0530

    "Add input component"

---
+++ Frontend/src/Components/Input.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { InputData } from "../types/types";
+
+const Input: React.FC<InputData> = ({
+  label,
+  type,
+  onChange,
+  value,
+  style,
+}) => {
+  return (
+    <>
+      <div className="w-full my-2">
+        <label
+          className={`${
+            style == "credentials"
+              ? "text-gray-300 font-normal text-sm"
+              : "text-white font-bold text-md"
+          }`}>
+          {label}
+        </label>
+        <br />
+        <input
+          type={type}
+          value={value}
+          onChange={onChange}
+          className=" remove-arrow  w-full  h-8 my-2 bg-transparent border-gray-200 border-2 rounded-xl text-gray-200 p-1 appearance-none 
+           font-medium text-sm tracking-wider focus:border-orange-400 outline-none duration-200 ease-out
+          "
+        />
+      </div>
+    </>
+  );
+};
+
+export default Input;
